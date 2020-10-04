e01645fd5c079f737014e302581e3b20ca623bb2
Karadjordje <sveljod@gmail.com>
2020-10-04 16:15:07 +0200

    Updates

---
+++ precache-manifest.60be73fa24165c7263fbf37337e8ebda.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f32fc4b1507aba9429f99c61cb0d0670",
+    "url": "/visa-card/index.html"
+  },
+  {
+    "revision": "b33090048c55d48a2ede",
+    "url": "/visa-card/static/css/main.511ec872.chunk.css"
+  },
+  {
+    "revision": "fe899cc92a3dfe6d55ea",
+    "url": "/visa-card/static/js/2.f4788ea8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/visa-card/static/js/2.f4788ea8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b33090048c55d48a2ede",
+    "url": "/visa-card/static/js/main.865bb9af.chunk.js"
+  },
+  {
+    "revision": "eb8c8c2322ede9e1a2ef",
+    "url": "/visa-card/static/js/runtime-main.99070332.js"
+  },
+  {
+    "revision": "c993dbe12a541cd7c193084d39c5d2d0",
+    "url": "/visa-card/static/media/chip.c993dbe1.png"
+  },
+  {
+    "revision": "feb5c6626819e02767862d50a09b206b",
+    "url": "/visa-card/static/media/master-card.feb5c662.png"
+  }
+]);
